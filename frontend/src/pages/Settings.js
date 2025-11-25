ba8af17f134aca0bf9d9a4ac068eec4a7e8424c3
Shiv <shivu7sm@hotmail.com>
2025-11-25 11:23:28 +0000

    auto-commit for be7cee36-8f04-4a8c-8d78-5ec9ee73ab34

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import Layout from '@/components/Layout';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
